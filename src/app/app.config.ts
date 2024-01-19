5793fd58f9742193b38a70cc321bb978ffe16bdc
Tidiane <tidianedevrtidiane@gmail.com>
2024-01-19 17:31:36 +0100

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import localeFr from '@angular/common/locales/fr';
-
-registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
-function registerLocaleData(localeFr: any, arg1: string) {
-  throw new Error('Function not implemented.');
-}
 
