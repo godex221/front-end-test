b0e5ce5c6ce45a2e37b78409d4cfcd638536cc92
Tidiane <tidianedevrtidiane@gmail.com>
2024-01-19 16:51:07 +0100

    update

--- src/features/sells/sells.component.ts
+++ src/features/sells/sells.component.ts
@@ -1,9 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { DxButtonModule, DxDataGridModule, DxSelectBoxModule } from 'devextreme-angular';
+import { DxDataGridModule, DxSelectBoxModule } from 'devextreme-angular';
 import { Ventes } from '../../utils/data';
 import { SELL } from '../../utils/interfaces';
-import dxDataGrid from 'devextreme/ui/data_grid';
 
 @Component({
   selector: 'app-sells',
